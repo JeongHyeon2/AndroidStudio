5647e97cca51d49273b025ff8a6d871073e6d596
JeongHyeon2 <3575253@naver.com>
2022-03-26 14:51:24 +0900

    2022.03.26네트워크 인터페이스 구축

--- ManageMentApplication/database/src/MemberManagement.java
+++ ManageMentApplication/database/src/MemberManagement.java
@@ -1,19 +1,78 @@
+import java.io.*;
 import java.util.ArrayList;
 
 public class MemberManagement {
     private ArrayList<Member> memberArr;
-    public MemberManagement(){
+    private String path;
+
+    public MemberManagement() {
         memberArr = new ArrayList<>();
+        path="C:\\Users\\35752\\Desktop\\MemberList.txt";
+        readFile();
     }
-    public void add(String id, String pwd, String name, int age){
-        memberArr.add(new Member(id,pwd,name,age));
+    public boolean login(String id, String pwd){
+        for(int i=0;i<memberArr.size();i++){
+            if(memberArr.get(i).getId().equals(id))
+                if(memberArr.get(i).getPassword().equals(pwd))return true;
+        }
+        return false;
     }
-    public void delete(String id){
+
+    public void add(String id, String pwd, String name, int age) throws DuplicationException {
         for(int i=0;i<memberArr.size();i++){
             if(memberArr.get(i).getId().equals(id)){
+                throw new DuplicationException();
+            }
+        }
+        memberArr.add(new Member(id, pwd, name, age));
+        saveFile(id+" "+pwd+" "+name+" "+age+"\n");
+    }
+    public void add_(String id, String pwd, String name, int age) {
+        memberArr.add(new Member(id, pwd, name, age));
+
+    }
+
+    public void delete(String id) {
+        for (int i = 0; i < memberArr.size(); i++) {
+            if (memberArr.get(i).getId().equals(id)) {
                 memberArr.remove(i);
             }
         }
     }
 
+    public void saveFile(String str) {
+        System.out.println("Saved to " + path);
+        File f =  new File(path);
+        try {
+            FileWriter fw = new FileWriter(f,true);
+            fw.write(str);
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+    public void readFile() {
+        System.out.println("Loaded from " + path);
+        File file = new File(path);
+        try {
+            if (file.isFile()) {
+                BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+                String s = "";
+                while ((s = br.readLine()) != null) {
+                    String[] arr = s.split(" ");
+                    add_(arr[0],arr[1],arr[2],Integer.parseInt(arr[3]));
+                }
+                br.close();
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
 }
+class DuplicationException extends Exception{
+
+}
+
--- ManageMentApplication/database/src/Server.java
+++ ManageMentApplication/database/src/Server.java
@@ -1,36 +1,74 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Arrays;
 
-public class Server {
-    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        ServerSocket sSocket = new ServerSocket(3003);
-        System.out.println("클라이언트 접속 대기중...");
-        Socket socket = sSocket.accept();
-        System.out.println("클라이언트 접속");
+public class Server extends Thread {
+    private Socket socket;
+    private MemberManagement management;
+    private OutputStream os;
+    private InputStream is;
 
-        OutputStream os = socket.getOutputStream();
-        InputStream is = socket.getInputStream();
-
-
-
-        while (true) {
-            Protocol protocol = new Protocol(); // 새 Protocol 객체 생성
-            byte[] buf = protocol.getPacket();
-            is.read(buf); // 클라이언트로부터 단어,정수 수신
-            int packetType = buf[0]; // 수신 데이터에서 패킷 타입 얻음
-            String[] arr =new String(buf).split("/");
-            for(int i=1;i<arr.length-1;i++) System.out.println(arr[i]);
-
-            switch (packetType) {
-                case Protocol.REQ_ADD:
-                    break;
-
-            }
+    public Server(Socket connectedClientSocket) {
+        this.socket = connectedClientSocket;
+        management = new MemberManagement();
+        try {
+            os = socket.getOutputStream();
+            is = socket.getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
+
+
+    public void run() {
+        try {
+//            ServerSocket sSocket = new ServerSocket(3003);
+//            System.out.println("클라이언트 접속 대기중...");
+//            Socket socket = sSocket.accept();
+//            System.out.println("클라이언트 접속");
+
+
+            while (true) {
+                Protocol protocol = new Protocol(); // 새 Protocol 객체 생성
+                byte[] buf = protocol.getPacket();
+                is.read(buf); // 클라이언트로부터 수신
+                int packetType = buf[0]; // 수신 데이터에서 패킷 타입 얻음
+                String[] arr = new String(buf).split("/");
+                for (int i = 1; i < arr.length - 1; i++) System.out.print(arr[i]+" ");
+
+                switch (packetType) {
+                    case Protocol.REQ_ADD:
+                        try {
+                            management.add(arr[1], arr[2], arr[3], Integer.parseInt(arr[4]));
+                        }catch (DuplicationException e){
+                            Protocol protocol1 = new Protocol(Protocol.DUP_ID);
+                            os.write(protocol1.getPacket());
+                            System.out.println("id 증복");
+                            break;
+                        }
+                        Protocol protocol1 = new Protocol(Protocol.SUCCESS);
+                        os.write(protocol1.getPacket());
+                        System.out.println("성공");
+                        break;
+                    case Protocol.REQ_LOGIN:
+                        System.out.println("id:"+arr[1]+" pwd:"+arr[2]);
+                        if(management.login(arr[1],arr[2])){
+                            Protocol protocol2 = new Protocol(Protocol.SUCCESS_LOGIN);
+                            os.write(protocol2.getPacket());
+                            System.out.println(arr[1]+" 로그인 성공");
+                        }else{
+                            Protocol protocol2 = new Protocol(Protocol.FAIL);
+                            os.write(protocol2.getPacket());
+                            System.out.println(arr[1]+" 로그인 실패");
+                        }
+                        break;
+
+                }
+            }
+        } catch (Exception e) {
+
+        }
+    }
+
 }
